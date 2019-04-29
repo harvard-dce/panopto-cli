945dbae3879c9dabda0a42d276994fdf6cb040cd
Jay Luker <jay_luker@harvard.edu>
2019-04-29 14:50:44 -0400

    setuptools integration

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+install_requires = [
+    "Click",
+    "requests",
+    "zeep"
+]
+
+setup(
+    name="panopto-cli",
+    version="0.1.0",
+    py_modules=['panopto'],
+    install_requires=install_requires,
+    licence='Apache 2.0',
+    author='Jay Luker',
+    url='https://github.com/harvard-dce/panopto-cli',
+    entry_points='''
+        [console_scripts]
+        panopto=panopto:cli 
+    ''',
+)
