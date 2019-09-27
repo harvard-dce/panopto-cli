5b87a2044124e8dcb702def7b19c153133a75dab
Jay Luker <jay_luker@harvard.edu>
2019-09-27 11:42:38 -0400

    version bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ install_requires = [
 
 setup(
     name="panopto-cli",
-    version="0.7.0",
+    version="0.7.1",
     py_modules=['panopto'],
     install_requires=install_requires,
     licence='Apache 2.0',
